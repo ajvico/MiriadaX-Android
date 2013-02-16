e7d8021422d6428c0122b9a316d5eb8db495972b
Ángel J. Vico <ajvico@gmail.com>
2013-02-16 20:08:42 +0100

    Módulo 3  - Se asocia el escuchador del evento onClick por código en lugar de    mediante un atributo en el archivo XML del diseño.

--- Asteroides/src/com/ajvico/asteroides/Asteroides.java
+++ Asteroides/src/com/ajvico/asteroides/Asteroides.java
@@ -1,21 +1,37 @@
 package com.ajvico.asteroides;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class Asteroides
    extends Activity
 {
+   // Referencia para el botón "Sobre el juego".
+   // Se usa para añadirle un escuchador para el evento onClick
+   private Button bAcercaDe;
+
 
    @Override
    protected void onCreate(Bundle savedInstanceState)
    {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.main);
+
+      // Se añade un escuchador para el evento onClick del botón "Sobre el juego"
+      bAcercaDe = (Button) findViewById(R.id.btnInfo);
+      bAcercaDe.setOnClickListener(new OnClickListener()
+      {
+         public void onClick(View view)
+         {
+            lanzarAcercaDe(view);
+         }
+      });
    }
 
 
