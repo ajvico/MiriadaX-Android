fb7784af2b9e4dd942f999909ad2f7a6ef9a52d5
ajvico <ajvico@server.fake>
2013-02-18 22:02:47 +0100

    Módulo 3  - Se controlan las pulsaciones sobre la lista de pulsaciones, mostrando    un mensaje temporal con los datos.

--- Asteroides/src/com/ajvico/asteroides/Puntuaciones.java
+++ Asteroides/src/com/ajvico/asteroides/Puntuaciones.java
@@ -2,6 +2,9 @@ package com.ajvico.asteroides;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class Puntuaciones
@@ -15,5 +18,44 @@ public class Puntuaciones
       setListAdapter(new MiAdaptador(
          this,
          Asteroides.almacen.listaPuntuaciones(10)));
+   }
+
+
+   /**
+    * Se ejecuta cuando se pulsa sobre un elemento del ListView que contiene
+    * esta actividad.
+    * 
+    * @param listView
+    *        Lista sobre la que se ha pulsado.
+    * @param view
+    *        Elemento de la lista sobre el que se ha pulsado.
+    * @param position
+    *        Posición que ocupa el elemento sobre el que se ha pulsado en la
+    *        lista.
+    * @param id
+    *        Identificador del elemento sobre el que se ha pulsado
+    * @see android.app.ListActivity#onListItemClick(android.widget.ListView,
+    *      android.view.View, int, long)
+    */
+   @Override
+   protected void onListItemClick(
+      ListView listView,
+      View view,
+      int position,
+      long id)
+   {
+      // Llamamos al método de la clase padre
+      super.onListItemClick(listView, view, position, id);
+
+      // Obtenemos el texto asociado al elemento sobre el que se ha pulsado
+      Object o = getListAdapter().getItem(position);
+
+      // Creamos un mensaje informativo con los datos del elemento sobre el que
+      // se ha pulsado y lo mostramos
+      Toast.makeText(
+         this,
+         getResources().getString(R.string.textoSeleccionPuntuacion) + ": "
+            + Integer.toString(position) + " - " + o.toString(),
+         Toast.LENGTH_LONG).show();
    }
 }
