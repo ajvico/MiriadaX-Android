571ac040e7572a7eaae3ca3aaed6396274fb9fbe
ajvico <ajvico@server.fake>
2013-03-03 17:40:11 +0100

    Módulo 5  - Se añade un hilo para actualizar la posición de los elementos del    juego.

--- Asteroides/src/com/ajvico/asteroides/VistaJuego.java
+++ Asteroides/src/com/ajvico/asteroides/VistaJuego.java
@@ -54,11 +54,27 @@ public class VistaJuego
     */
    private int numAsteroides = 5;
 
-   //
    /**
     * Fragmentos en que se divide cada asteroide.
     */
    private int numFragmentos = 3;
+
+   /* THREAD Y TIEMPO */
+
+   /**
+    * Thread encargado de procesar el juego.
+    */
+   private ThreadJuego thread = new ThreadJuego();
+
+   /**
+    * Cada cuanto queremos procesar cambios (ms).
+    */
+   private static int PERIODO_PROCESO = 50;
+
+   /**
+    * Cuando se realizó el último proceso.
+    */
+   private long ultimoProceso = 0;
 
 
    /**
@@ -140,6 +156,10 @@ public class VistaJuego
          }
          while (asteroide.distancia(nave) < (ancho + alto) / 5);
       }
+      
+      // Iniciamos el hilo que controla los elementos del juego
+      ultimoProceso = System.currentTimeMillis();
+      thread.start();
    }
 
 
@@ -149,7 +169,7 @@ public class VistaJuego
     * @see android.view.View#onDraw(android.graphics.Canvas)
     */
    @Override
-   protected void onDraw(Canvas canvas)
+   protected synchronized void onDraw(Canvas canvas)
    {
       super.onDraw(canvas);
 
@@ -162,4 +182,67 @@ public class VistaJuego
          asteroide.dibujaGrafico(canvas);
       }
    }
-}
+
+
+   /**
+    * Actualiza la posición de todos los elementos del juego.
+    * Calcula la velocidad y dirección de la nave a partir de la información
+    * proporcionada por el jugador mediante los controles del juego.
+    */
+   protected synchronized void actualizaFisica()
+   {
+      long ahora = System.currentTimeMillis();
+
+      // No hagas nada si el período de proceso no se ha cumplido.
+      if (ultimoProceso + PERIODO_PROCESO > ahora)
+      {
+         return;
+      }
+
+      // Para una ejecución en tiempo real calculamos retardo
+      double retardo = (ahora - ultimoProceso) / PERIODO_PROCESO;
+      ultimoProceso = ahora; // Para la próxima vez
+
+      // Actualizamos velocidad y dirección de la nave a partir de giroNave y
+      // aceleracionNave (según la entrada del jugador)
+      nave.setAngulo((int) (nave.getAngulo() + giroNave * retardo));
+      double nIncX =
+         nave.getIncX() + aceleracionNave
+            * Math.cos(Math.toRadians(nave.getAngulo())) * retardo;
+      double nIncY =
+         nave.getIncY() + aceleracionNave
+            * Math.sin(Math.toRadians(nave.getAngulo())) * retardo;
+
+      // Actualizamos si el módulo de la velocidad no excede el máximo
+      if (Math.hypot(nIncX, nIncY) <= Grafico.getMaxVelocidad())
+      {
+         nave.setIncX(nIncX);
+         nave.setIncY(nIncY);
+      }
+
+      // Actualizamos posiciones X e Y
+      nave.incrementaPos(retardo);
+      for (Grafico asteroide : asteroides)
+      {
+         asteroide.incrementaPos(retardo);
+      }
+   }
+
+
+   /**
+    * Hilo para actualizar la posición de los elementos de juego en segundo
+    * plano
+    */
+   class ThreadJuego
+      extends Thread
+   {
+      @Override
+      public void run()
+      {
+         while (true)
+         {
+            actualizaFisica();
+         }
+      }
+   } // class ThreadJuego
+} // class VistaJuego
