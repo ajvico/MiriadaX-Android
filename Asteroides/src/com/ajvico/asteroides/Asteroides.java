4c0f14bbac45140b2fe3ec1ed5f93e2dbf0e46bc
ajvico <ajvico@server.fake>
2013-03-10 19:36:31 +0100

    Módulo 6  - Se añaden mensajes Toast para mostrar los eventos del ciclo de vida    de la actividad principal a medida que se van ejecutando.

--- Asteroides/src/com/ajvico/asteroides/Asteroides.java
+++ Asteroides/src/com/ajvico/asteroides/Asteroides.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -38,6 +39,8 @@ public class Asteroides
    {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.main);
+
+      Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
 
       // Se añade un escuchador para el evento onClick del botón
       // "Sobre el juego"
@@ -82,6 +85,54 @@ public class Asteroides
                lanzarJuego(v);
             }
          });
+   }
+
+
+   @Override
+   protected void onStart()
+   {
+      super.onStart();
+      Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+   }
+
+
+   @Override
+   protected void onResume()
+   {
+      super.onResume();
+      Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+   }
+
+
+   @Override
+   protected void onPause()
+   {
+      Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+      super.onPause();
+   }
+
+
+   @Override
+   protected void onStop()
+   {
+      Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+      super.onStop();
+   }
+
+
+   @Override
+   protected void onRestart()
+   {
+      super.onRestart();
+      Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+   }
+
+
+   @Override
+   protected void onDestroy()
+   {
+      Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+      super.onDestroy();
    }
 
 
